3fa127415eddc2c8a61ff365b546e537690d881a
RazdolbayOne <Yone312@gmail.com>
2021-06-28 17:46:42 +0300

    new tutorial, now rect is moving

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include<filesystem>
 #include<string>
 #include<SFML/Graphics.hpp> //sf namespace
-
+#include<SFML/Audio.hpp>
 
 constexpr auto FRAME_WIDTH = 680;
 constexpr auto FRAME_HEIGHT = 480;
@@ -15,65 +15,60 @@ namespace fs = std::filesystem;
 
 int main()
 {
+
+
 	// create the window
 	sf::RenderWindow  window(sf::VideoMode(FRAME_WIDTH, FRAME_HEIGHT), "First app", sf::Style::Close | sf::Style::Resize);
-	
-	sf::CircleShape pepeShape(SHAPE_SIZE);
-	sf::Texture textureShape;
-
-	fs::path pic_path{ "./res/background.jpg" };
-	pic_path.make_preferred(); //makes to specific OS path
-
-	if (!textureShape.loadFromFile(pic_path.string()))
-	{
-		std::cout << "the texture does not load\n";
-		return -1;
-	}
-
-	pepeShape.setTexture(&textureShape);
-	pepeShape.setPosition((FRAME_WIDTH / 2) - (SHAPE_SIZE / 2), (FRAME_HEIGHT / 2) - (SHAPE_SIZE / 2));
-
-	//set picture size
-	pepeShape.setOrigin(SHAPE_SIZE, SHAPE_SIZE);
-
-	//background stuff
-	sf::Sprite backgroundSprite;
-	sf::Vector2 background_size = textureShape.getSize();
-
-
-	//TODO need to make if background picture is smaller than screen size
-	backgroundSprite.setTexture(textureShape);
-	backgroundSprite.scale(sf::Vector2f(static_cast<float>(FRAME_WIDTH)/ background_size.x,
-										static_cast<float>(FRAME_HEIGHT)/ background_size.y));
-
+	sf::RectangleShape player(sf::Vector2f(SHAPE_SIZE, SHAPE_SIZE));
+	player.setFillColor(sf::Color::Green);
 
 	// run the program as long as the window is open
 	while (window.isOpen()) {
-		sf::Event event;
+		sf::Event evnt;
+		
 		//event loop
-		while (window.pollEvent(event)) {
-			// "close requested" event: we close the window
+		while (window.pollEvent(evnt)) {
+			
 
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-			else if (event.type == sf::Event::KeyPressed)
+			switch (evnt.type)
 			{
-
-				if (event.key.code == sf::Keyboard::Right)
-				{
-					pepeShape.rotate(10.f);
-				}
-				else if (event.key.code == sf::Keyboard::Left) {
-					pepeShape.rotate(-10.f);
-				}
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::TextEntered:
+				std::printf("%c", evnt.text.unicode);
+				//break;
+				break;
+			default:
+				break;
+				//return -1;
 			}
 		}
 
-		window.clear();
-		window.draw(backgroundSprite);
-		window.draw(pepeShape);
+		//arrows control
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			player.move(0.1f,0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			player.move(0.0f, 0.1f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			player.move(-0.1f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			player.move(0.0f,-0.1f);
+		}
 
+
+
+
+		window.clear();
+
+		window.draw(player);
 		//end the current frame;
 		window.display();
 	}
